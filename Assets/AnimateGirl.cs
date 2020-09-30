f1852c2f2d37cf1cc4038a852f1c796588983308
Ayce45 <evan.j@hotmail.fr>
2020-09-30 16:08:21 +0200

    first commit

---
+++ Assets/AnimateGirl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(SpriteRenderer))]
+public class AnimateGirl : MonoBehaviour
+{
+    [Tooltip("Vitesse max en unité par seconde")]
+    public int MaxSpeed = 10;
+    Animator animator;
+    SpriteRenderer mySpriteRenderer;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    static readonly int Speed = Animator.StringToHash("Speed");
+
+    void Update()
+    {
+        var maxDistancePerFrame = MaxSpeed * Time.deltaTime;
+        Vector2 speed = Vector2.zero;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetFloat("Speed", 1f);
+            mySpriteRenderer.flipX = false;
+            speed += Vector2.right * maxDistancePerFrame;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            animator.SetFloat("Speed", 2f);
+            mySpriteRenderer.flipX = true;
+            speed += Vector2.left * maxDistancePerFrame;
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetFloat("Speed", 3f);
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetFloat("Speed", 4f);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.SetFloat("Speed", 5f);
+            speed += Vector2.up * maxDistancePerFrame;
+            mySpriteRenderer.flipY = false;
+
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            animator.SetFloat("Speed", 5f);
+            speed += Vector2.down * maxDistancePerFrame;
+            mySpriteRenderer.flipY = true;
+
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0f);
+            mySpriteRenderer.flipY = false;
+        }
+        this.transform.position = this.transform.position + (Vector3)speed;
+    }
+}
---
+++ Assets/FollowGirl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowGirl : MonoBehaviour
+{
+    [Tooltip("Target element")]
+    public GameObject target;
+    [Tooltip("deadZone in unite")]
+    public int deadZone;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (this.transform.position.x - target.transform.position.x > deadZone)
+        {
+            this.transform.position = new Vector3(target.transform.position.x + deadZone, this.transform.position.y, this.transform.position.z);
+        }
+        else if (this.transform.position.x - target.transform.position.x < -deadZone)
+        {
+            this.transform.position = new Vector3(target.transform.position.x - deadZone, this.transform.position.y, this.transform.position.z);
+        }
+
+        if (this.transform.position.y - target.transform.position.y > deadZone)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, target.transform.position.y + deadZone, this.transform.position.z);
+        }
+        else if (this.transform.position.y - target.transform.position.y < -deadZone)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, target.transform.position.y - deadZone, this.transform.position.z);
+
+        }
+    }
+}
